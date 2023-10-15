72f54da4cf1ed32a3a5acae6d2289d00b88112ee
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-10-16 00:00:50 +0330

    #

--- day2/STARTER-FILES/index.js
+++ day2/STARTER-FILES/index.js
@@ -1,11 +1,3 @@
-const inCartBtn = document.querySelectorAll(".add-btn");
-const checkIcon = document.querySelector(".check-icon")
+const ulDiv = document.querySelector(".menu");
 
-inCartBtn.forEach(btn=>{
-    btn.addEventListener("click", (e)=>{
-        btn.innerHTML ="In Cart";
-        checkIcon.style.display ="block"
-       
-    })
-})
-
+men
