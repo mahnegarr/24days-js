c07b818e53d2353a6873a4d59c606e725a114887
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-10-17 01:14:58 +0330

    #

--- day2/STARTER-FILES/index.js
+++ day2/STARTER-FILES/index.js
@@ -1,10 +1,13 @@
 import { menuItems } from "./app.js";
 const panel = document.querySelector(".panel");
-const panelCart = document.querySelector(".panel cart")
+const panelCart = document.getElementById("panel-cart");
+const quantityWrapper = document.querySelectorAll(".quantity__wrapper");
 
-function addItem(menuItems) {
+let clickedItems = [];
+
+function addItem(items) {
   let ul;
-  menuItems.forEach((item) => {
+  items.forEach((item) => {
     ul = document.createElement("ul");
     ul.classList.add("menu");
     ul.innerHTML = `
@@ -22,28 +25,30 @@ function addItem(menuItems) {
     panel.appendChild(ul);
   });
 }
-let clickedItems =[];
 function addToCart() {
   const allBtn = document.querySelectorAll(".add");
   allBtn.forEach((btn) => {
     btn.addEventListener("click", (e) => {
       const id = e.target.dataset.id;
-       const clickedItem = menuItems.find((i) => i.id === parseInt(id));
-       clickedItems.push(clickedItem)
+      const clickedItem = menuItems.find((i) => i.id === parseInt(id));
+      clickedItems.push(clickedItem);
       btn.innerHTML = `<button class="in-cart">
             <img src="images/check.svg" alt="Check" />
             In Cart
             </button>`;
+      showInCart();
     });
   });
 }
 
 function showInCart() {
-    clickedItems.forEach(item=>{
-        const ul = document.createElement("ul")
-        ul.classList.add(".cart-summary")
+  let ul;
+  panelCart.innerHTML = "";
+  clickedItems.forEach((item) => {
+    ul = document.createElement("ul");
+    ul.classList.add("cart-summary");
 
-        ul.innerHTML=`<li>
+    ul.innerHTML = `<li>
         <div class="plate">
           <img src=${item.image} alt=${item.alt} class="plate" />
           <div class="quantity">${item.count}</div>
@@ -53,24 +58,41 @@ function showInCart() {
           <p class="price">$${item.price}</p>
         </div>
         <div class="quantity__wrapper">
-          <button class="decrease">
-            <img src="images/chevron.svg" />
+          <button class="decrease" >
+            <img src="images/chevron.svg" class="down" data-id=${item.id} />
           </button>
-          <div class="quantity">1</div>
+          <div class="quantity">${item.count}</div>
           <button class="increase">
-            <img src="images/chevron.svg" />
+            <img src="images/chevron.svg" class="up" data-id=${item.id}/>
           </button>
         </div>
         <div class="subtotal">
-          $6.34
+          $ ${item.price * item.count}
         </div>
-      </li>`
-      panelCart.appendChild(ul)
-    })
+
+        </li>`;
+    panelCart.appendChild(ul);
+  });
+  increaseItems();
 }
 
+function increaseItems() {
+  quantityWrapper.forEach((wrapper) => {
+    wrapper.addEventListener("click", (e) => {
+      if (e.target.classList.contains("down")) {
+        console.log(e.target);
+        let id = e.target.dataset.id;
+        const item = menuItems.find((i) => i.id === parseInt(id));
+        item.count++;
+    
+
+        showInCart();
+      }
+    });
+  });
+}
 window.addEventListener("DOMContentLoaded", () => {
   addItem(menuItems);
   addToCart();
-  showInCart()
+  showInCart();
 });
