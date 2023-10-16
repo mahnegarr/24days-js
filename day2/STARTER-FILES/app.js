a467410dd75e3549fdd949d63deac924850cb3d9
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-10-16 17:14:29 +0330

    #

--- day2/STARTER-FILES/app.js
+++ day2/STARTER-FILES/app.js
@@ -1,7 +1,7 @@
 export const menuItems = [
     {
         name: 'French Fries with Ketchup',
-        price: 223,
+        price: 2.23,
         image: './images/plate__french-fries.png',
         alt: 'French Fries',
         count: 0,
@@ -9,7 +9,7 @@ export const menuItems = [
     },
     {
         name: 'Salmon and Vegetables',
-        price: 512,
+        price: 5.12,
         image: './images/plate__salmon-vegetables.png',
         alt: 'Salmon and Vegetables',
         count: 0,
@@ -17,7 +17,7 @@ export const menuItems = [
     },
     {
         name: 'Spaghetti Meat Sauce',
-        price: 782,
+        price: 7.82,
         image: './images/plate__spaghetti-meat-sauce.png',
         alt: 'Spaghetti with Meat Sauce',
         count: 0,
@@ -25,7 +25,7 @@ export const menuItems = [
     },
     {
         name: 'Bacon, Eggs, and Toast',
-        price: 599,
+        price: 5.99,
         image: './images/plate__bacon-eggs.png',
         alt: 'Bacon, Eggs, and Toast',
         count: 0,
@@ -33,7 +33,7 @@ export const menuItems = [
     },
     {
         name: 'Chicken Salad with Parmesan',
-        price: 698,
+        price: 6.98,
         image: './images/plate__chicken-salad.png',
         alt: 'Chicken Salad with Parmesan',
         count: 0,
@@ -41,7 +41,7 @@ export const menuItems = [
     },
     {
         name: 'Fish Sticks and Fries',
-        price: 634,
+        price: 6.34,
         image: './images/plate__fish-sticks-fries.png',
         alt: 'Fish Sticks and Fries',
         count: 0,
--- day2/STARTER-FILES/index.js
+++ day2/STARTER-FILES/index.js
@@ -1,24 +1,76 @@
-// import {menuItems} from "./app.js"
-// const ulDiv = document.querySelector(".menu");
+import { menuItems } from "./app.js";
+const panel = document.querySelector(".panel");
+const panelCart = document.querySelector(".panel cart")
 
-// function addItem(menuItems){
-//     let result=""
-//     menuItems.forEach(item => {
-//         result+= `<li>
-//         <div class="plate">
-//           <img src=${item.image} alt="Fish Sticks and Fries" class="plate" />
-//         </div>
-//         <div class="content">
-//           <p class="menu-item">${item.name}</p>
-//           <p class="price">$${item.price}</p>
-//           <button class="add" data-id=${item.id}>Add to Cart</button>
-//         </div>
-//       </li>`
-//     });
-//     ulDiv.innerHTML=result
-    
-// }
+function addItem(menuItems) {
+  let ul;
+  menuItems.forEach((item) => {
+    ul = document.createElement("ul");
+    ul.classList.add("menu");
+    ul.innerHTML = `
+        <li>
+            <div class="plate">
+                <img src=${item.image} alt=${item.alt} class="plate" />
+            </div>
+            <div class="content">
+                <p class="menu-item">${item.name}</p>
+                <p class="price">$${item.price}</p>
+                <button class="add" data-id=${item.id}>Add to Cart</button>
+            </div>
+   </li> `;
 
-// window.addEventListener("DOMContentLoaded",()=>{
-//     addItem(menuItems)
-// })
+    panel.appendChild(ul);
+  });
+}
+let clickedItems =[];
+function addToCart() {
+  const allBtn = document.querySelectorAll(".add");
+  allBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const id = e.target.dataset.id;
+       const clickedItem = menuItems.find((i) => i.id === parseInt(id));
+       clickedItems.push(clickedItem)
+      btn.innerHTML = `<button class="in-cart">
+            <img src="images/check.svg" alt="Check" />
+            In Cart
+            </button>`;
+    });
+  });
+}
+
+function showInCart() {
+    clickedItems.forEach(item=>{
+        const ul = document.createElement("ul")
+        ul.classList.add(".cart-summary")
+
+        ul.innerHTML=`<li>
+        <div class="plate">
+          <img src=${item.image} alt=${item.alt} class="plate" />
+          <div class="quantity">${item.count}</div>
+        </div>
+        <div class="content">
+          <p class="menu-item">${item.name}</p>
+          <p class="price">$${item.price}</p>
+        </div>
+        <div class="quantity__wrapper">
+          <button class="decrease">
+            <img src="images/chevron.svg" />
+          </button>
+          <div class="quantity">1</div>
+          <button class="increase">
+            <img src="images/chevron.svg" />
+          </button>
+        </div>
+        <div class="subtotal">
+          $6.34
+        </div>
+      </li>`
+      panelCart.appendChild(ul)
+    })
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  addItem(menuItems);
+  addToCart();
+  showInCart()
+});
