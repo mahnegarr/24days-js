3b68e2b8be8d2573ac39dfaf4fa8a0136737ff77
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-10-18 19:06:09 +0330

    #

--- day2/STARTER-FILES/app.js
+++ day2/STARTER-FILES/app.js
@@ -1,50 +1,50 @@
 export const menuItems = [
-    {
-        name: 'French Fries with Ketchup',
-        price: 2.23,
-        image: './images/plate__french-fries.png',
-        alt: 'French Fries',
-        count: 0,
-        id:1
-    },
-    {
-        name: 'Salmon and Vegetables',
-        price: 5.12,
-        image: './images/plate__salmon-vegetables.png',
-        alt: 'Salmon and Vegetables',
-        count: 0,
-        id:2
-    },
-    {
-        name: 'Spaghetti Meat Sauce',
-        price: 7.82,
-        image: './images/plate__spaghetti-meat-sauce.png',
-        alt: 'Spaghetti with Meat Sauce',
-        count: 0,
-        id:3
-    },
-    {
-        name: 'Bacon, Eggs, and Toast',
-        price: 5.99,
-        image: './images/plate__bacon-eggs.png',
-        alt: 'Bacon, Eggs, and Toast',
-        count: 0,
-        id:4
-    },
-    {
-        name: 'Chicken Salad with Parmesan',
-        price: 6.98,
-        image: './images/plate__chicken-salad.png',
-        alt: 'Chicken Salad with Parmesan',
-        count: 0,
-        id:5
-    },
-    {
-        name: 'Fish Sticks and Fries',
-        price: 6.34,
-        image: './images/plate__fish-sticks-fries.png',
-        alt: 'Fish Sticks and Fries',
-        count: 0,
-        id:6
-    }
-]
+  {
+    name: "French Fries with Ketchup",
+    price: 2.23,
+    image: "./images/plate__french-fries.png",
+    alt: "French Fries",
+    count: 0,
+    id: 1,
+  },
+  {
+    name: "Salmon and Vegetables",
+    price: 5.12,
+    image: "./images/plate__salmon-vegetables.png",
+    alt: "Salmon and Vegetables",
+    count: 0,
+    id: 2,
+  },
+  {
+    name: "Spaghetti Meat Sauce",
+    price: 7.82,
+    image: "./images/plate__spaghetti-meat-sauce.png",
+    alt: "Spaghetti with Meat Sauce",
+    count: 0,
+    id: 3,
+  },
+  {
+    name: "Bacon, Eggs, and Toast",
+    price: 5.99,
+    image: "./images/plate__bacon-eggs.png",
+    alt: "Bacon, Eggs, and Toast",
+    count: 0,
+    id: 4,
+  },
+  {
+    name: "Chicken Salad with Parmesan",
+    price: 6.98,
+    image: "./images/plate__chicken-salad.png",
+    alt: "Chicken Salad with Parmesan",
+    count: 0,
+    id: 5,
+  },
+  {
+    name: "Fish Sticks and Fries",
+    price: 6.34,
+    image: "./images/plate__fish-sticks-fries.png",
+    alt: "Fish Sticks and Fries",
+    count: 0,
+    id: 6,
+  },
+];
--- day2/STARTER-FILES/index.js
+++ day2/STARTER-FILES/index.js
@@ -1,8 +1,9 @@
 import { menuItems } from "./app.js";
 const panel = document.querySelector(".panel");
 const panelCart = document.getElementById("panel-cart");
-const panelContainer = document.getElementById("container");
-
+const subtotalDiv = document.querySelector(".price.subtotal");
+const TaxDiv = document.querySelector(".price.tax");
+const totalDiv = document.querySelector(".price.total");
 let clickedItems = [];
 
 function addItem(items) {
@@ -31,6 +32,7 @@ function addToCart() {
     btn.addEventListener("click", (e) => {
       const id = e.target.dataset.id;
       const clickedItem = menuItems.find((i) => i.id === parseInt(id));
+      clickedItem.count = 1;
       clickedItems.push(clickedItem);
       btn.innerHTML = `<button class="in-cart">
             <img src="images/check.svg" alt="Check" />
@@ -40,11 +42,17 @@ function addToCart() {
     });
   });
 }
-let total =[]
+let subtotal = 0;
+let priceTax = 0;
+let total = 0;
 function showInCart() {
   panelCart.innerHTML = "";
   let ul;
   clickedItems.forEach((item) => {
+    const itemTotal = item.price * item.count;
+    subtotal = itemTotal;
+    priceTax = subtotal * 0.09;
+    total = subtotal + priceTax;
     ul = document.createElement("ul");
     ul.classList.add("cart-summary");
 
@@ -67,36 +75,17 @@ function showInCart() {
     </button>
     </div>
     <div class="subtotal">
-     $${(item.price * item.count).toFixed(2)}
+    $${(item.price * item.count).toFixed(2)}
     </div>
     </li>`;
-    total.push(item.price * item.count.toFixed(2))
     panelCart.appendChild(ul);
+    subtotalDiv.innerHTML = `$${subtotal.toFixed(2)}`;
+    TaxDiv.innerHTML = `$${priceTax.toFixed(2)}`;
+    totalDiv.innerHTML = `$${total.toFixed(2)}`;
   });
-  let totals;
-  clickedItems.forEach((item) => {
-    totals = document.createElement("div");
-    totals.classList.add("totals");
 
-    totals.innerHTML = `<div class="totals">
-    <div class="line-item">
-      <div class="label">Subtotal:${total}</div>
-      <div class="amount price subtotal">$10.80</div>
-    </div>
-    <div class="line-item">
-      <div class="label">Tax:</div>
-      <div class="amount price tax">$1.05</div>
-    </div>
-    <div class="line-item total">
-      <div class="label">Total:</div>
-      <div class="amount price total">$11.85</div>
-    </div>
-  </div>`;
-  panelContainer.appendChild(totals)
-  });
   increaseItems();
 }
-
 function increaseItems() {
   const upBtns = document.querySelectorAll(".up");
   const quantity = document.querySelector(".quantity");
@@ -106,13 +95,14 @@ function increaseItems() {
       let item = menuItems.find((i) => i.id === parseInt(id));
       item.count++;
       quantity.innerHTML = item.count;
-
       showInCart();
     });
   });
 }
+
 window.addEventListener("DOMContentLoaded", () => {
   addItem(menuItems);
   addToCart();
   showInCart();
 });
+
