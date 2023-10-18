a185325f30ff2fd39ce63c28326bfef8bf3d0520
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-10-18 22:34:15 +0330

    #

--- Day7/STARTER-FILES/app.js
+++ Day7/STARTER-FILES/app.js
@@ -1,5 +1,32 @@
 const billAmountInput = document.getElementById("bill-amount");
-const peopleNumInput = document.getElementById("number-of-people")
-const tipAmountInput = document.getElementById("tip-amount");
+const peopleNumInput = document.getElementById("number-of-people");
+const tipAmount = document.getElementById("tip-amount");
 const totalPerPerson = document.getElementById("total-per-person");
+const calcBtn = document.getElementById("calculate");
 
+let billValue;
+let peopleNumValue;
+
+billAmountInput.addEventListener("input", (e) => {
+  billValue = e.target.value;
+  calculateTip();
+});
+
+peopleNumInput.addEventListener("input", (e) => {
+  peopleNumValue = e.target.value;
+});
+const allTips = document.querySelectorAll('input[name="tip"]');
+
+function calculateTip() {
+  allTips.forEach((tip) => {
+    tip.addEventListener("click", (e) => {
+      selectedTip = e.target.value;
+      tip = (billValue * selectedTip.split("%")[0]) / 100;
+      let totalBill = (parseFloat(billValue) + tip) / parseInt(peopleNumValue);
+      calcBtn.addEventListener("click", () => {
+        tipAmount.innerHTML = tip.toFixed(2);
+        totalPerPerson.innerHTML = totalBill.toFixed(2);
+      });
+    });
+  });
+}
