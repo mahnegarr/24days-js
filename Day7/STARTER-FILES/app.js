8e81b93ff469c2fe4d34ea90af1f2c65b08f780b
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-10-18 21:46:51 +0330

    #

---
+++ Day7/STARTER-FILES/app.js
@@ -0,0 +1,5 @@
+const billAmountInput = document.getElementById("bill-amount");
+const peopleNumInput = document.getElementById("number-of-people")
+const tipAmountInput = document.getElementById("tip-amount");
+const totalPerPerson = document.getElementById("total-per-person");
+
