bdb28a8470c9be22ae3559daa269964be85b0814
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-10-20 19:23:22 +0330

    #

--- day13/13__PROJECT-FILES/STARTER-FILES/index.js
+++ day13/13__PROJECT-FILES/STARTER-FILES/index.js
@@ -1,11 +1,19 @@
 const closeBtn = document.querySelector(".close");
 const overlay = document.querySelector(".overlay");
+const dotBtn = document.querySelector(".dot");
+const modal = document.querySelector(".modal");
 
-closeBtn.addEventListener("click",(e)=>{
-    console.log(e.target);
-})
+closeBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-overlay.addEventListener("click",()=>{
-    document.querySelector('.modal').style.display = 'none';
-    overlay.style.display = "none"
-})
+function closeModal() {
+  modal.style.display = "none";
+  overlay.style.display = "none";
+  modal.classList.remove("centered");
+}
+
+dotBtn.addEventListener("click", () => {
+  overlay.style.display = "block";
+  modal.style.display = "block";
+  modal.classList.add("centered");
+});
