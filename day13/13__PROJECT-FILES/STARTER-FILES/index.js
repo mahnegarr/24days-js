7d95bf31704fac13190d65a09f08eae6b7e8633e
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-10-20 18:51:55 +0330

    #

--- day13/13__PROJECT-FILES/STARTER-FILES/index.js
+++ day13/13__PROJECT-FILES/STARTER-FILES/index.js
@@ -1,1 +1,11 @@
 const closeBtn = document.querySelector(".close");
+const overlay = document.querySelector(".overlay");
+
+closeBtn.addEventListener("click",(e)=>{
+    console.log(e.target);
+})
+
+overlay.addEventListener("click",()=>{
+    document.querySelector('.modal').style.display = 'none';
+    overlay.style.display = "none"
+})
